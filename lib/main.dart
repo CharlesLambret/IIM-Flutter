287a948d6ceb5bfe5fc89b8ecb6216c674291fb9
charles <charles@penguin>
2023-06-13 14:40:55 +0200

    rendu

--- lib/main.dart
+++ lib/main.dart
@@ -14,32 +14,55 @@ void main() {
 class MyApp extends StatelessWidget {
   final List<Experience> experiences = [
     Experience(
-      company: 'Google',
-      position: 'Software Engineer',
-      period: 'Jan 2020 - Dec 2021',
-      description: 'Developed software solutions',
+      company: 'Erium',
+      position: 'Apprenti | Chef de Projet Digital ',
+      period: 'Juin 2022 - Septembre 2023',
+      description: 'Production & maintien des supports du produit Cyber Investigation, compte-rendu de données clients, satisfaction client. Travail en collaboration avec le Chef de Projet Digital, le Directeur de service et l’équipe produit.',
+    ),
+    Experience(
+      company: 'Expairs | Bed Boat | Night Square',
+      position: 'Freelance | Chef de Projet Digital ',
+      period: 'Juillet 2020 - Aujourd\'hui',
+      description: 'Travail seul ou en collaboration avec un autre chef de projet digital afin de produire différents livrables : parcours utilisateurs et maquettes de sites, réalisations no-code.',
+    ),
+    Experience(
+      company: 'Seed',
+      position: 'Stage | Chef de Projet Digital ',
+      period: 'Juillet 2021 - Août 2021',
+      description: 'Travail en collaboration direct avec le président de l’entreprise pour effectuer des veilles juridiques & sectorielles, mettre à niveau la comptabilité de l’entreprise & ses outils numériques.',
     ),
   ];
 
   final List<Skill> skills = [
     Skill(name: 'Flutter'),
-    Skill(name: 'Dart'),
+    Skill(name: 'Python'),
+    Skill(name: 'React'),
+    Skill(name: 'Node'),
+    Skill(name: 'Laravel'),
+    Skill(name: 'Analytics'),
+    Skill(name: 'Amplitude'),
+    Skill(name: 'Trello'),
+    Skill(name: 'Airtable'),
+    Skill(name: 'Zapier'),
+    Skill(name: 'Suite Adobe'),
+    Skill(name: 'Analytics'),
+    Skill(name: 'Notion'),
   ];
 
   final List<ExperienceAssos> experiencesassos = [
     ExperienceAssos(
-      association: 'test 1',
-      role: 'test',
-      period: '(Année - Année)',
-      description: 'test'
+      association: 'Repairs!94 | Repairs!75 - Mairie de Paris | Léo & Co',
+      role: 'Bénévole | Chef de projet digital',
+      period: '2020-2023',
+      description: 'Travail seul ou en collaboration avec une équipe de freelances afin de livrer différents produits du même type que mes productions freelances'
     ),
   ];
 
   final List<Education> educations = [
     Education(
-      institution: 'Ecole 1',
-      degree: 'Diplôme',
-      period: '(Année - Année)',
+      institution: 'IIM',
+      degree: 'Bachelor Coding & Digital Innovation',
+      period: '2020-2023',
     ),
   ];
 
@@ -50,13 +73,14 @@ class MyApp extends StatelessWidget {
     ),
     Language(
       name: 'Anglais',
-      level: 'Courant',
+      level: 'C1',
     ),
   ];
 
   final List<Hobby> hobbies = [
+    Hobby(name: 'Guitare'),
+    Hobby(name: 'Jeux-vidéo'),
     Hobby(name: 'Lecture'),
-    Hobby(name: 'Voyage'),
   ];
 
   @override
@@ -66,10 +90,13 @@ class MyApp extends StatelessWidget {
         title: Text('CV Charles Lambret'),
       ),
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(40.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+        padding: EdgeInsets.all(20.0),
+        child: Center(
+          child: FractionallySizedBox(
+            widthFactor: 0.8, // Utilisez 80% de la largeur du parent
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
             CircleAvatar(
               radius: 50,
               backgroundImage: NetworkImage('https://media.licdn.com/dms/image/C4E03AQGK1HJx4pl2og/profile-displayphoto-shrink_400_400/0/1663072684919?e=1692230400&v=beta&t=-l4FpsWeU6Ih8Ub-_ao4LWcjutNqr3YA_9fZza1cbT4'), // Image from the internet
@@ -114,7 +141,8 @@ class MyApp extends StatelessWidget {
 
             HobbySection(hobbies: hobbies)
           ],
-          
+            )
+          )
         ),
       ),
     );
--- lib/skills.dart
+++ lib/skills.dart
@@ -5,7 +5,7 @@ class Skill {
   Skill({required this.name});
 }
 
-class SkillsSection extends StatelessWidget{
+class SkillsSection extends StatelessWidget {
   final List<Skill> skills;
 
   SkillsSection({Key? key, required this.skills}) : super(key: key);
@@ -16,22 +16,32 @@ class SkillsSection extends StatelessWidget{
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-              'Compétences : ',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-        ListView.builder(
+          'Compétences : ',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        GridView.builder(
           shrinkWrap: true,
           physics: NeverScrollableScrollPhysics(),
           itemCount: skills.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            childAspectRatio: 4.0, 
+          ),
           itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(
-                skills[index].name,
+            return Container(
+              padding: EdgeInsets.all(5),
+              child: Center(
+                child: Text(
+                  skills[index].name,
+                  style: TextStyle(fontSize: 18),
+                ),
               ),
             );
           },
         )
-      ]
+      ],
     );
   }
 }
